2f7817cc7806ab01a688b1230b14e8094be579f7
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-24 12:31:38 +0300

    configure imports in views and update urls

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from awwards.models import Profile, Project
+from .forms import CreateUserForm,ProfileForm,ProjectForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout as dj_login
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Myprofile,Myprojects
+from .serializer import MerchSerializer
 
 # Create your views here.
