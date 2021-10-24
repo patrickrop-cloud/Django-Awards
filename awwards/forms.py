de66f9414355977c1adcb0c09a6ef4ac655f26c0
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-24 14:06:49 +0300

    add project form class

--- awwards/forms.py
+++ awwards/forms.py
@@ -7,3 +7,10 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model =  Project
+        exclude = ['username', 'design', 'usability', 'creativity','overall_score','screenshot2','screenshot3']
+
+        
