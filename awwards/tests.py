63ae09aeb26553632eb44b9872072e6d2c146c35
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-25 14:42:41 +0300

    add test to test if our project is saved correctlt

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile,Project
 
 # Create your tests here.
+
+#setup method
+class ProjectTestclass(TestCase):
+    def setUp(self):
+        self.projects = Project(title = 'mytitle', description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
+
+#Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.projects,Project))
