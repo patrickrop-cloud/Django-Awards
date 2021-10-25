8f5ccad5772911139420c6d656534e25b6b40d40
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-25 22:05:55 +0300

    deploy

--- project/settings.py
+++ project/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 MODE = config("MODE", default = "dev")
+
 SECRET_KEY = config('SECRET_KEY') 
 
 
