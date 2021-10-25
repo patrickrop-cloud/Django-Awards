3965d6d0583640f8ff8a0bcb7e80c065bbbfff5d
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-25 14:56:47 +0300

    debug test to run

--- awwards/tests.py
+++ awwards/tests.py
@@ -7,8 +7,48 @@ from .models import Profile,Project
 #setup method
 class ProjectTestclass(TestCase):
     def setUp(self):
-        self.projects = Project(title = 'mytitle', description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
+        self.projects = Project(title = 'title', description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
 
 #Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.projects,Project))
+    
+
+ #save method
+    # def test_save_project(self):
+    #     self.projects.save_project()
+    #     projects=Project.objects.all()
+    #     self.assertTrue(len(projects)>0) 
+
+
+#delete method
+    # def test_delete_project(self):
+    #     self.projects.save_project()
+    #     project_record=Project.objects.all()
+    #     self.projects.delete_project()
+    #     self.assertTrue(len(project_record)==0)
+
+
+class ProfileTestclass(TestCase):
+    #setup method
+    def setUp(self):
+        self.myprofile=Profile(bio='Hardworking',email='email@gmail.com',avatar='avatar')
+
+     #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.myprofile,Profile))
+
+    
+    #save method
+    def test_save_profile(self):
+        self.myprofile.save_profile()
+        profile=Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_profile(self):
+        self.myprofile.save_profile()
+        profile_record=Profile.objects.all()
+        self.myprofile.delete_profile()
+        self.assertTrue(len(profile_record)==0)
+
+
