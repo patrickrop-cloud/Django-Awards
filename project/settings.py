5774a56317a7fc4bb2e206101c3940772223a143
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-25 10:25:54 +0300

    add navbar and also configured settings

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +83,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'patrick',
+        'PASSWORD': 'patrick'
     }
 }
 
@@ -139,3 +142,6 @@ STATICFILES_DIRS = (
 )
 
 STAR_RATINGS_RERATE = False
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
