525e8c3a2dc502c511cc034c4b27e243bf653383
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-25 09:18:58 +0300

    add profile, project models

--- awwards/models.py
+++ awwards/models.py
@@ -21,3 +21,28 @@ class Project(models.Model):
 
     def delete_project(self):
         self.delete()
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    bio = TextField(max_length=500,null=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,blank=True)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+        
+class Myprojects(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+
+class Myprofile(models.Model):
+    name = models.CharField(max_length=40)
+    bio = models.TextField()
+    email = models.EmailField()
